7f7e19197117c0e248904ae4ad470f5d6449165a
Axel <axel.ericsson@gmail.com>
2013-04-24 14:25:22 +0200

    test again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tjinga::Application.config.secret_token = 'c5507538f6f99dccc7eba79713ffe1d4a0fb688ddad25d2c7f773a36099078c193496f64f0da281684ecd10e5a962fde5db383ca279076c1cb1f8d07aacc91ca'
